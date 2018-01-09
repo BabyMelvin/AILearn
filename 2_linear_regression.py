0fb2f5cbd25d4c2e406bea30a116c5a191213b23
BabyMelvin <caohang@henggucn.com>
2018-01-09 14:22:19 +0800

    base concept tensorflow

---
+++ 2_linear_regression.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+"""
+    linear regression model:线性回归模型
+    optimizers: slowly change each variable to minimize the loss function
+    simplest optimizer:gradient descent(梯度下降)
+        gradient descent:
+                modify each variable accord to the magnitude of the derivative(导数大小) of the loss with respect to that variable
+                    根据这个变量损失导数大小的方向
+"""
+# model parameters
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+
+# model input and output 相对于loss(model)来说input 和 output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y))
+
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)  # reset values to wrong
+for i in range(1000):
+    sess.run(train, {x: x_train, y: y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
+print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
---
+++ 3_estimator.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+"""
+    simplify the mechanics of machine learn:
+        run training loop
+        run evaluation loop
+        manage data sets
+"""
+# simpler the linear regression program
+# Numpy is often used to load,manipulate and pre_process data
+import numpy as np
+
+# declare list of features.we only have one numeric feature
+# there are many other types of columns that are more complicated and useful
+feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
+
+# a estimator is the front end to invoke training(fitting) and evaluation(inference)
+# there are many pre_defined types like linear regression,linear classification ,and many neural network classifiers and regressors
+# linear regression
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+
+# TensorFlow provides many helper method to read and set up data sets.
+# here we use two data sets:one for training and one for evaluation
+# we have to tell the function how many batched of data(num_epochs多少次) and how big data batch (每次多长)should be
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True
+)
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False
+)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False
+)
+
+# we can invoke 1000 training step by invoking the method and passing the training data set
+estimator.train(input_fn=input_fn, steps=1000)
+
+# here we evaluate how well our module did
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics:%r" % train_metrics)
+print("eval metrics:%r" % eval_metrics)
